dad8700c56ba6f30905374ca694250ed277cb6f1
gusmonov <rabdavinci@yandex.ru>
2016-09-26 12:24:42 +0500

    test

--- views/layouts/_calcPopup.php
+++ views/layouts/_calcPopup.php
@@ -14,6 +14,7 @@ use yii\helpers\Url;
 $model = new CalculateDelivery();
 ?>
 
+
 <div class="btn-popup">
     <a href="#" class="close-popup">Close</a>
     <div class="popup">
@@ -50,3 +51,5 @@ $model = new CalculateDelivery();
 
     </div>
 </div>
+
+
