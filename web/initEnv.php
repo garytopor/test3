4a43b417e33c73eb98829929199dd045a5fa7908
gusmonov <rabdavinci@yandex.ru>
2016-09-27 23:38:57 +0500

    rebuild

--- web/initEnv.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Инициализация окружения приложения
- */
-
-// Раскомментировть на деве:
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
-//defined('YII_ENV') or define('YII_ENV', 'prod');
